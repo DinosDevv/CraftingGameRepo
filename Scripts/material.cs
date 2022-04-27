acdaecdd49d2477d4bc252fa9fb6025edaf4c4d4
DinosDevv <92534008+DinosDevv@users.noreply.github.com>
2022-04-27 09:55:55 +0300

    Add files via upload

---
+++ Scripts/material.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Material", menuName = "Material Object")]
+public class material : ScriptableObject
+{
+    public string matName;
+    public Sprite sprite;
+
+}
